a0154bab4cc7b28361d861f95eee81067a52ce5d
Meghal Aggarwal <meghal.aggarwal2201@gmail.com>
2021-08-27 18:05:22 -0700

    Binary Search leetcode Square Root Problem

---
+++ src/easy/SquareRoot.java
@@ -0,0 +1,36 @@
+package easy;
+
+public class SquareRoot {
+    public static void main(String[] args) {
+        int target = 0;
+        SquareRoot sr = new SquareRoot();
+        System.out.println(sr.mySqrt(target));
+    }
+
+    public int mySqrt(int x) {
+
+        long left = 1;
+        long right = x/2, sqrt = 0;
+
+        if(x == 0 || x ==1)
+            return x;
+
+        else {
+            while (left <= right) {
+                long mid = left + (right - left) / 2;
+
+                if ((mid * mid) > x) {
+                    right = (int) mid - 1;
+                } else
+                    left = mid + 1;
+                sqrt = mid;
+            }
+
+            if (sqrt * sqrt > x)
+                return (int) sqrt - 1;
+            else
+                return (int) sqrt;
+        }
+    }
+}
+
